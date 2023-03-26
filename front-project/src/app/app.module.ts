2d9b37cf9cb1a6d6f96b51a1db54c1dec32ba1a0
kacperlechicki <klechicki@poczta.fm>
2023-03-26 22:35:17 +0200

    basic imports

--- front-project/src/app/app.module.ts
+++ front-project/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { NgxLoadingModule } from 'ngx-loading';
+import { ToastrModule } from 'ngx-toastr';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    NgxLoadingModule.forRoot({
+      primaryColour: '#FCA311',
+      secondaryColour: '#FCA311',
+      tertiaryColour: '#FCA311'
+    }),
+    ToastrModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
