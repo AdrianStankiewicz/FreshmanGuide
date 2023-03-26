d99e7f8ab65fcca3f9afad673240d81f0482018a
KacperLechicki <klechicki.wrk@gmail.com>
2023-03-26 22:39:38 +0200

    basic imports (#4)

--- front-project/src/app/app.component.ts
+++ front-project/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'front-project';
+
+  constructor(private toast: ToastrService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.toast.success('Test');
+  }
 }
