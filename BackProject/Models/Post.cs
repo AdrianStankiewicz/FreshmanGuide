002d07427c60908c79570356cc3386fa9359f7ce
Oliwia <liwiamilka@gmail.com>
2023-04-26 19:42:44 +0200

    Some small touches to models.

--- BackProject/Models/Post.cs
+++ BackProject/Models/Post.cs
@@ -6,7 +6,7 @@ namespace BackProject.Models
     {
         public int Id { get; set; }
         public String Nick { get; set; }
-        public Category CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public String Body { get; set; }
         public DateTime CreatedAt { get; set; }
         public Boolean Verified { get; set; }
