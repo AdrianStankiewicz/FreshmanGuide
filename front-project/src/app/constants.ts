882d2edbebabe37a67560882687f81b300e3b12c
Oliwia <liwiamilka@gmail.com>
2023-05-04 20:53:38 +0200

    Connects front to Azure backend.

--- front-project/src/app/constants.ts
+++ front-project/src/app/constants.ts
@@ -2,5 +2,9 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class Constants{
-  public static backendApiUrl: string = "https://localhost:7088/api/";
+  // Local backend
+  // public static backendApiUrl: string = "https://localhost:7088/api/";
+
+  //Azure
+  public static backendApiUrl: string = "https://freshmanguidebackend.azurewebsites.net/api/";
 }
