5bd14e4d1e9d53679fc8bb976f6c111cc68c722c
KarolFalkowski <99480997+KarolFalkowski@users.noreply.github.com>
2023-05-14 14:39:11 +0200

    Added connection to database (#25)

--- BackProject/Models/Post.cs
+++ BackProject/Models/Post.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BackProject.Models
 {
     public class Post
     {
         public int Id { get; set; }
+        [Required]
         public String Nick { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public String Body { get; set; }
         public DateTime CreatedAt { get; set; }
         public Boolean Verified { get; set; }
-
-        public List<Reply> Replies { get; set; }
+        public List<Reply> Reply { get; set; }
     }
 }
--- BackProject/Program.cs
+++ BackProject/Program.cs
@@ -1,3 +1,6 @@
+using BackProject.Db;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyAppDbContext>(options => options.UseSqlServer
+    (builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 
 var app = builder.Build();
 
