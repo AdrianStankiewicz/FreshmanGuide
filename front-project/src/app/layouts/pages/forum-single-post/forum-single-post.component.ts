82d0f19811c8b0f0d21650f4d53b32807ca592ab
KacperLechicki <klechicki@poczta.fm>
2023-05-28 16:52:54 +0200

    Adding replies works (#31)

--- front-project/src/app/layouts/pages/forum-single-post/forum-single-post.component.ts
+++ front-project/src/app/layouts/pages/forum-single-post/forum-single-post.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post';
 import { Reply } from 'src/app/models/reply';
@@ -9,11 +9,18 @@ import { LoadingService } from 'src/app/services/loading.service';
 import { ReplyPopupComponent } from '../../partials/reply-popup/reply-popup.component';
 import { CategoriesService } from 'src/app/services/http/categories.service';
 import { Category } from 'src/app/models/category';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  PageEvent,
+} from '@angular/material/paginator';
+import { CustomPaginator } from 'src/app/custom-paginator-configuration';
 
 @Component({
   selector: 'app-forum-single-post',
   templateUrl: './forum-single-post.component.html',
   styleUrls: ['./forum-single-post.component.css'],
+  providers: [{ provide: MatPaginatorIntl, useValue: CustomPaginator() }],
 })
 export class ForumSinglePostComponent {
   loading!: boolean;
@@ -22,18 +29,23 @@ export class ForumSinglePostComponent {
   postData!: Post;
   postID!: number;
   postCategory!: Category;
+  slicedReplies: Reply[] = [];
+  pageSize: number = 3;
 
   filteredReplies: Reply[] = [];
   numberOfReplies: number = 0;
 
   selectedVerified: string = '';
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(
     private loadingService: LoadingService,
     private route: ActivatedRoute,
     private postsService: PostsService,
     private dialogRef: MatDialog,
-    private categoriesService: CategoriesService
+    private categoriesService: CategoriesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -45,13 +57,14 @@ export class ForumSinglePostComponent {
         .getOneFromPosts(data['id'])
         .subscribe(async (post: Post): Promise<void> => {
           this.postData = post;
+          this.filteredReplies = this.postData.reply;
+          this.slicedReplies = this.filteredReplies.slice(0, this.pageSize);
+          this.numberOfReplies = this.filteredReplies.length;
           this.getPostCategorySub = this.categoriesService
             .getOneFromCategories(this.postData.categoryId)
             .subscribe((category: Category): void => {
               this.postCategory = category;
             });
-
-          this.filteredReplies = this.postData.reply;
         });
     });
 
@@ -82,6 +95,11 @@ export class ForumSinglePostComponent {
 
     this.filteredReplies = filteredReplies;
     this.numberOfReplies = this.filteredReplies.length;
+    this.paginator.firstPage();
+    this.slicedReplies = this.filteredReplies;
+    this.paginator.length = this.numberOfReplies;
+    this.paginator.pageSize = this.pageSize;
+    this.slicedReplies = this.filteredReplies.slice(0, this.pageSize);
   }
 
   onVerifiedChange(event: any): void {
@@ -91,10 +109,23 @@ export class ForumSinglePostComponent {
 
   openDialog(): void {
     const dialogConfig: MatDialogConfig<any> = new MatDialogConfig();
+    const urlParams = this.router.url;
     dialogConfig.disableClose = true;
     dialogConfig.hasBackdrop = true;
     dialogConfig.width = 'clamp(310px, 95%, 800px)';
+    dialogConfig.data = {
+      params: urlParams,
+    };
 
     this.dialogRef.open(ReplyPopupComponent, dialogConfig);
+  }
+
+  onPageChange(event: PageEvent): void {
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.filteredReplies.length) {
+      endIndex = this.filteredReplies.length;
+    }
+    this.slicedReplies = this.filteredReplies.slice(startIndex, endIndex);
   }
 }
