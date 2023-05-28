408dec7ff86967d84baf6bc2cce027e0d98b883c
Kamila Wlodarska <k.wlodarska0@gmail.com>
2023-05-28 21:41:08 +0200

    internship page without select category working

--- front-project/src/app/layouts/pages/internship/internship.component.ts
+++ front-project/src/app/layouts/pages/internship/internship.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Internship } from 'src/app/models/internship';
 import { InternshipsService } from 'src/app/services/http/internships.service';
 import { LoadingService } from 'src/app/services/loading.service';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  PageEvent,
+} from '@angular/material/paginator';
+import { CustomPaginator } from 'src/app/custom-paginator-configuration';
 
 @Component({
   selector: 'app-internship',
   templateUrl: './internship.component.html',
   styleUrls: ['./internship.component.css'],
+  providers: [{ provide: MatPaginatorIntl, useValue: CustomPaginator() }],
 })
 export class InternshipComponent {
   loading!: boolean;
   internships: Internship[] = [];
+  numberOfInternships: number = 0;
+  filteredInternships: Internship[] = [];
+  slicedInternships: Internship[] = [];
+  pageSize: number = 3;
+
+  paginatorText!: any;
+
+  searchKeyword: string = '';
+  searchedInternships: Internship[] = [];
+
+  selectedCategory: string = '';
 
   getAllFromInternshipsSub!: Subscription;
 
@@ -20,13 +38,20 @@ export class InternshipComponent {
     private internshipsService: InternshipsService
   ) {}
 
+  @ViewChild('categorySelectElement') categorySelectElement!: ElementRef;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   ngOnInit(): void {
     this.loading = this.loadingService.startLoading();
+    window.scrollTo(0, 0);
 
     this.getAllFromInternshipsSub = this.internshipsService
       .getAllFromInternships()
       .subscribe((internships: Internship[]): void => {
         this.internships = internships;
+        this.filteredInternships = this.internships;
+        this.slicedInternships = this.filteredInternships.slice(0, this.pageSize);
+        this.numberOfInternships = this.filteredInternships.length;
       });
   }
 
@@ -38,5 +63,56 @@ export class InternshipComponent {
 
   ngOnDestroy(): void {
     this.loading = this.loadingService.startLoading();
+  }
+
+  onSearchChange(): void {
+    this.searchedInternships = this.filteredInternships;
+    this.applyFilters();
+  }
+
+  onCategoryChange(event: any): void {
+    this.selectedCategory = event.target.value;
+    this.applyFilters();
+  }
+
+  applyFilters(): void {
+    let filteredInternships: Internship[] = this.internships;
+
+    if (this.searchKeyword.trim() !== '') {
+      filteredInternships = filteredInternships.filter((internship): boolean =>
+      internship.name.toLowerCase().includes(this.searchKeyword.toLowerCase())
+      );
+    }
+
+    this.filteredInternships = filteredInternships;
+    this.numberOfInternships = this.filteredInternships.length;
+    this.paginator.firstPage();
+    this.slicedInternships = this.filteredInternships;
+    this.paginator.length = this.numberOfInternships;
+    this.paginator.pageSize = this.pageSize;
+    this.slicedInternships = this.filteredInternships.slice(0, this.pageSize);
+  }
+
+  onReset(): void {
+    this.searchKeyword = '';
+    this.selectedCategory = '';
+
+    this.filteredInternships = this.internships;
+    this.filteredInternships.sort((a: Internship, b: Internship): number => a.id - b.id);
+    this.numberOfInternships = this.filteredInternships.length;
+
+    this.paginator.pageSize = this.pageSize;
+    this.slicedInternships = this.filteredInternships.slice(0, this.pageSize);
+
+    this.categorySelectElement.nativeElement.value = '';
+  }
+
+  onPageChange(event: PageEvent): void {
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+    if (endIndex > this.filteredInternships.length) {
+      endIndex = this.filteredInternships.length;
+    }
+    this.slicedInternships = this.filteredInternships.slice(startIndex, endIndex);
   }
 }
