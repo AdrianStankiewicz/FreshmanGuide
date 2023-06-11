e960ee4290523d3b700bfad3eac8cc12dbd6fde5
Oliwia Milewska <99407556+OliwiaMilewska@users.noreply.github.com>
2023-06-11 21:16:11 +0200

    Adds Google Analytics (#41)

--- front-project/src/app/app.component.ts
+++ front-project/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-project';
+
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    navEndEvents.subscribe((event: any) => {
+      gtag('config', 'G-5K71JPYRF2', {
+        'page-path': event.urlAfterRedirects
+      });
+    });
+  }
 }
