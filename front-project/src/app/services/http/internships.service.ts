93118496e8f0bca8dab5c3a9fbee75504f981c37
Kamila Wlodarska <k.wlodarska0@gmail.com>
2023-10-28 20:44:09 +0200

    Adding postInternship to service

--- front-project/src/app/services/http/internships.service.ts
+++ front-project/src/app/services/http/internships.service.ts
@@ -1,14 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, retry, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, retry, throwError } from 'rxjs';
 import { Constants } from 'src/app/constants';
-import { Internship } from 'src/app/models/internship';
+import { Internship, PostInternship } from 'src/app/models/internship';
 import { HandleErrorService } from '../handle-error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InternshipsService {
+  public allInternships$ = new BehaviorSubject<Internship[]>([]);
+
+  public getAllPosts$(): Observable<Internship[]> {
+    return this.allInternships$.asObservable();
+  }
+
   constructor(
     private http: HttpClient,
     private handleErrorService: HandleErrorService
@@ -23,8 +29,13 @@ export class InternshipsService {
   public getOneFromInternships(internshipID: number): Observable<Internship> {
     return this.http
       .get<Internship>(
-        `${Constants.backendApiUrl}Main/GetInternship/${internshipID}`
-      )
+        `${Constants.backendApiUrl}Main/GetInternship/${internshipID}`)
       .pipe(retry(1), catchError(this.handleErrorService.handleError));
+  }
+
+  public postInternship(internship: PostInternship): Observable<Internship> {
+    return this.http
+      .post<Internship>(`${Constants.backendApiUrl}Main/PostInternship`, internship)
+      .pipe(catchError(this.handleErrorService.handleError));
   }
 }
