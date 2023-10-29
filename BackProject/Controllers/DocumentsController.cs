8ea921de60071d5da4179d2a4fb0b153de74947e
Oliwia <liwiamilka@gmail.com>
2023-10-29 14:57:26 +0100

    Backend changes to store and retrive documents.

---
+++ BackProject/Controllers/DocumentsController.cs
@@ -0,0 +1,69 @@
+﻿using BackProject.Db;
+using BackProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly MyAppDbContext _context;
+
+        public DocumentsController(MyAppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDocuments()
+        {
+            try
+            {
+                var docs = _context.Document.ToList();
+                if (docs.Count == 0)
+                {
+                    return NotFound("No element found");
+                }
+                return Ok(docs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("{title}")]
+        public async Task<IActionResult> UploadDocument(IFormFile file, string title)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return BadRequest("File not selected");
+
+                using (var ms = new MemoryStream())
+                {
+                    await file.CopyToAsync(ms);
+                    var fileBytes = ms.ToArray();
+
+                    var fileExtension = Path.GetExtension(file.FileName).TrimStart('.');
+
+                    var document = new Document
+                    {
+                        Doc = fileBytes,
+                        Extension = fileExtension,
+                        Title = title
+                    };
+                    _context.Document.Add(document);
+                    await _context.SaveChangesAsync();
+
+                    return Ok("File uploaded successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BackProject/Models/Document.cs
@@ -0,0 +1,10 @@
+﻿namespace BackProject.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public byte[] Doc { get; set; }
+        public string Extension { get; set; }
+        public string Title { get; set; }
+    }
+}
