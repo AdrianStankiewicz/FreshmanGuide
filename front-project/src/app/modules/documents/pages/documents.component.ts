b85d5dd676bc0cd66cdf3fed64bc5ea006aaf25a
Oliwia <liwiamilka@gmail.com>
2023-11-05 11:43:17 +0100

    Adds documents service.

--- front-project/src/app/modules/documents/pages/documents.component.ts
+++ front-project/src/app/modules/documents/pages/documents.component.ts
@@ -1,49 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, distinctUntilChanged } from 'rxjs';
 import { Constants } from 'src/app/constants';
 import { DocumentV } from 'src/app/models/document';
+import { DocumentsService } from 'src/app/services/http/documents.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit, AfterViewInit, OnDestroy{
   documents: DocumentV[] = [];
+  private _subscriptions = new Subscription();
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private documentsService: DocumentsService,
+    private loadingService: LoadingService) { }
 
   ngOnInit(): void {
     this.getDocuments();
   }
 
   getDocuments(): void {
-    this.http.get<DocumentV[]>(`${Constants.backendApiUrl}Documents`).subscribe(
-      (data) => {
-        this.documents = data;
-      },
-      (error) => {
-        console.error('Error fetching documents: ', error);
-      }
+    this.loadingService.startLoading();
+
+    this._subscriptions.add(
+      this.documentsService
+        .getAllDocuments()
+        .pipe(distinctUntilChanged())
+        .subscribe((documents: DocumentV[]): void => {
+          this.documents = documents.sort();
+        })
     );
   }
 
   downloadDocument(documentId: number,title: string, extension: string): void {
-    this.http.get(`${Constants.backendApiUrl}Documents/DownloadDocument/` + documentId, { responseType: 'blob' }).subscribe(
-      (data) => {
-        const blob = new Blob([data], { type: 'application/octet-stream' });
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
+    this.documentsService.downloadDocument(documentId).subscribe((data) => {
+      const blob = new Blob([data], { type: 'application/octet-stream' });
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement('a');
         a.href = url;
         a.download = title + '.' + extension.trim();
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
         window.URL.revokeObjectURL(url);
-      },
-      (error) => {
-        console.error('Error downloading document: ', error);
-      }
-    );
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.loadingService.stopLoading();
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.unsubscribe();
   }
 }
