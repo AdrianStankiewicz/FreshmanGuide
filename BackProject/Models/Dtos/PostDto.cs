7a8f6b11a04f7c739caf3cca40bd4eef1645f3de
Oliwia <liwiamilka@gmail.com>
2023-11-07 17:52:50 +0100

    Endpoints to edit and delete posts.

---
+++ BackProject/Models/Dtos/PostDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackProject.Models.Dtos
+{
+    public class PostDto
+    {
+        public String Nick { get; set; }
+        public int CategoryId { get; set; }
+        public String Body { get; set; }
+        public Boolean Verified { get; set; }
+    }
+}
