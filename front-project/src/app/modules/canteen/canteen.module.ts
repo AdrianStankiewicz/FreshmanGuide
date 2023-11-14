8206d81d5a559ef6c07ff26689b9b8bd849c815a
kacperlechicki <klechicki@poczta.fm>
2023-11-14 17:11:53 +0100

    missing spinners

--- front-project/src/app/modules/canteen/canteen.module.ts
+++ front-project/src/app/modules/canteen/canteen.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CanteenRoutingModule } from './canteen-routing.module';
 import { CanteenComponent } from './pages/canteen.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [CanteenComponent],
-  imports: [CommonModule, CanteenRoutingModule],
+  imports: [CommonModule, CanteenRoutingModule, MatProgressSpinnerModule],
 })
 export class CanteenModule {}
--- front-project/src/app/modules/documents/pages/documents.component.ts
+++ front-project/src/app/modules/documents/pages/documents.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription, distinctUntilChanged } from 'rxjs';
-import { Constants } from 'src/app/constants';
 import { DocumentV } from 'src/app/models/document';
 import { DocumentsService } from 'src/app/services/http/documents.service';
 import { LoadingService } from 'src/app/services/loading.service';
@@ -9,21 +7,31 @@ import { LoadingService } from 'src/app/services/loading.service';
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.css']
+  styleUrls: ['./documents.component.css'],
 })
-export class DocumentsComponent implements OnInit, AfterViewInit, OnDestroy{
-  documents: DocumentV[] = [];
+export class DocumentsComponent implements OnInit, AfterViewInit, OnDestroy {
+  protected documents: DocumentV[] = [];
   private _subscriptions = new Subscription();
 
   constructor(
     private documentsService: DocumentsService,
-    private loadingService: LoadingService) { }
+    private loadingService: LoadingService
+  ) {}
 
   ngOnInit(): void {
+    this.loadingService.startLoading();
     this.getDocuments();
   }
 
-  getDocuments(): void {
+  ngAfterViewInit(): void {
+    this.loadingService.stopLoading();
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.unsubscribe();
+  }
+
+  private getDocuments(): void {
     this.loadingService.startLoading();
 
     this._subscriptions.add(
@@ -36,25 +44,21 @@ export class DocumentsComponent implements OnInit, AfterViewInit, OnDestroy{
     );
   }
 
-  downloadDocument(documentId: number,title: string, extension: string): void {
+  protected downloadDocument(
+    documentId: number,
+    title: string,
+    extension: string
+  ): void {
     this.documentsService.downloadDocument(documentId).subscribe((data) => {
       const blob = new Blob([data], { type: 'application/octet-stream' });
-        let url = window.URL.createObjectURL(blob);
-        let a = document.createElement('a');
-        a.href = url;
-        a.download = title + '.' + extension.trim();
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
+      let url = window.URL.createObjectURL(blob);
+      let a = document.createElement('a');
+      a.href = url;
+      a.download = title + '.' + extension.trim();
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
     });
-  }
-
-  ngAfterViewInit(): void {
-    this.loadingService.stopLoading();
-  }
-
-  ngOnDestroy(): void {
-    this._subscriptions.unsubscribe();
   }
 }
